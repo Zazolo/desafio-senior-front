7277504a702f459b6ea2d5e8f73a1adf57ca1502
Tiago Batista Corrêa <tiagobc@live.com>
2021-06-10 08:16:01 -0300

    Form start.

--- src/app/pages/form-page/form-page.component.ts
+++ src/app/pages/form-page/form-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, ActivatedRouteSnapshot, Route } from '@angular/router';
+import { IListTableData } from 'src/app/interfaces/ilist-table-data';
 
 @Component({
   selector: 'app-form-page',
@@ -9,6 +11,17 @@ import { ActivatedRoute, ActivatedRouteSnapshot, Route } from '@angular/router';
 export class FormPageComponent implements OnInit {
 
   id:string | null = '123456';
+
+  form:FormGroup = new FormGroup({
+    name: new FormControl(''),
+    measurement_unit:new FormControl(''),
+    quantity: new FormControl(''),
+    price:new FormControl(''),
+    perishable:new FormControl(''),
+    expiration_date: new FormControl(''),
+    manufacturing_date:new FormControl('')
+
+  });
 
   constructor(
     private routeSnap:ActivatedRoute
